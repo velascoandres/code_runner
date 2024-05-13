57d27981ce20fb73e34c87013d0c8830df40208d
Andres Velasco <velasco.andrs@gmail.com>
2024-05-12 20:23:15 -0500

    added adapters

--- code-templates/rust/target/debug/build/proc-macro2-d084d4f0d2bb29ce/out/proc_macro2.d
+++
@@ -1,7 +0,0 @@
-/Users/andresvelasco/Documents/proyectos/code-zone/code_runner/code-templates/rust/target/debug/build/proc-macro2-d084d4f0d2bb29ce/out/libproc_macro2.rmeta: build/probe.rs
-
-/Users/andresvelasco/Documents/proyectos/code-zone/code_runner/code-templates/rust/target/debug/build/proc-macro2-d084d4f0d2bb29ce/out/proc_macro2.d: build/probe.rs
-
-build/probe.rs:
-
-# env-dep:RUSTC_BOOTSTRAP
--- code-templates/rust/target/debug/deps/rust-aab5b9d2c51b70e0.d
+++
@@ -1,6 +0,0 @@
-/Users/andresvelasco/Documents/proyectos/code-zone/code_runner/code-templates/rust/target/debug/deps/rust-aab5b9d2c51b70e0: src/main.rs src/solution.rs
-
-/Users/andresvelasco/Documents/proyectos/code-zone/code_runner/code-templates/rust/target/debug/deps/rust-aab5b9d2c51b70e0.d: src/main.rs src/solution.rs
-
-src/main.rs:
-src/solution.rs:
